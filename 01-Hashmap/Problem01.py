4e972662b1dbc14c38e1cbc1e25aab4780a61a4c
Harshit <prajapatiharshit2004@gmail.com>
2025-12-11 23:21:03 +0530

    1,2 added

--- 01-Hashmap/Problem01.py
+++ 01-Hashmap/Problem01.py
@@ -1,1 +1,20 @@
-# started
+# 217 -> contains duplicate
+from typing import List
+
+def containsDuplicate(nums: List[int]) -> bool:
+        hash_set = {}
+        for n in nums:
+            if n in hash_set:
+                return True
+            else:
+                hash_set[n] = 1
+        return False
+
+print(containsDuplicate([1,2,3,4]))
+print(containsDuplicate([1,2,3,1]))
+
+
+"""
+Time = N
+space = N
+"""
---
+++ 01-Hashmap/Problem02.py
@@ -0,0 +1,20 @@
+# valid Anagram
+
+def isAnagram(s,t):
+    if len(s)!=len(t): return False
+    countmap = [0]*26
+
+    for i in s:
+        countmap[ord(i)-ord('a')] += 1
+    for j in t:
+        countmap[ord(j)-ord('a')] -= 1
+        if countmap[(ord(j)-ord('a'))] < 0:
+            return False
+    return True
+
+print(isAnagram("anagram","nagaram"))
+print(isAnagram("rat","car"))
+
+
+# time N
+# space O(26)
